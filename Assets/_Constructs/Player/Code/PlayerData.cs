c08f3c4ebb94df72c453a9237d07ebbab548bd6d
Riza "Risky" Resnick <riza@nysom.com>
2024-11-16 15:06:11 -0800

    Add Flow and Player constructs

---
+++ Assets/_Constructs/Player/Code/PlayerData.cs
@@ -0,0 +1,12 @@
+using Anarchy.Core.Common;
+using UnityEngine;
+
+namespace NameChangeSimulator.Constructs.Player
+{
+	[CreateAssetMenu(fileName = "PlayerData", menuName = "Anarchy/Constructs/Create PlayerData")]
+	public class PlayerData : AnarchyData
+	{
+		// Add public fields for this construct's data
+		// Go to Anarchy/Update Bindings to use events
+	}
+}
