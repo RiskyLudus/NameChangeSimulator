28fd48f5cbec8817cc077bf55160e0a792829758
Riza "Risky" Resnick <riza@nysom.com>
2024-11-16 20:27:58 -0800

    Update Fields to hold values

--- Assets/Scripts/Shared/Field.cs
+++ Assets/Scripts/Shared/Field.cs
@@ -5,7 +5,8 @@ using UnityEngine.UI;
 [Serializable]
 public class Field
 {
-    public string Name;
+    public string Name = string.Empty;
+    public string Value = string.Empty;
     public bool IsCheck = false;
     public bool IsText = false;
 }
