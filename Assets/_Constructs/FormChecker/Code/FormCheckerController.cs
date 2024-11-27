c349d90e2b9c6be37c3ed7d6f6d8390ed53b3647
XanNava <Alexanderxn78@Gmail.com>
2024-11-26 21:46:18 -0800

    Fix Web build error for Mono.Cecil library reference

--- Assets/_Constructs/FormChecker/Code/FormCheckerController.cs
+++ Assets/_Constructs/FormChecker/Code/FormCheckerController.cs
@@ -1,9 +1,6 @@
-using System;
+
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using Anarchy.Shared;
-using Mono.Cecil;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
