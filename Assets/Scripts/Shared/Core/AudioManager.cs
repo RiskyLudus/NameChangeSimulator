55998799d2690cc220edbb332ce1b1c8e25688fd
Riza "Risky" Resnick <riza@nysom.com>
2024-12-20 13:33:24 -0800

    Readd back in the VO to the AudioManager

--- Assets/Scripts/Shared/Core/AudioManager.cs
+++ Assets/Scripts/Shared/Core/AudioManager.cs
@@ -32,7 +32,21 @@ namespace NameChangeSimulator.Shared
         [SerializeField] private AudioClip uiHoverSound;
         [SerializeField] private AudioClip uiHoverExitSound;
 
-        // Other SFX and VO clips...
+        [Header("VO Clips")]
+        [SerializeField] private AudioClip voAh;
+        [SerializeField] private AudioClip voDisgust;
+        [SerializeField] private AudioClip voGuh;
+        [SerializeField] private AudioClip voGuh2;
+        [SerializeField] private AudioClip voLaugh;
+        [SerializeField] private AudioClip voMhm1;
+        [SerializeField] private AudioClip voMhm2;
+        [SerializeField] private AudioClip voMm1;
+        [SerializeField] private AudioClip voMm2;
+        [SerializeField] private AudioClip voMm3;
+        [SerializeField] private AudioClip voMmmm;
+        [SerializeField] private AudioClip voMmQ;
+        [SerializeField] private AudioClip voScoff;
+        [SerializeField] private AudioClip voSigh;
 
         protected override void Awake()
         {
@@ -137,6 +151,23 @@ namespace NameChangeSimulator.Shared
         public void PlayUIConfirm_SFX() => PlaySfx(uiConfirmSound);
         public void PlayUIHover_SFX() => PlaySfx(uiHoverSound);
         public void PlayUIHoverExit_SFX() => PlaySfx(uiHoverExitSound);
+        #endregion
+
+        #region VO Sounds
+        public void PlayVO_Ah() => PlayVoiceOver(voAh);
+        public void PlayVO_Disgust() => PlayVoiceOver(voDisgust);
+        public void PlayVO_Guh() => PlayVoiceOver(voGuh);
+        public void PlayVO_Guh2() => PlayVoiceOver(voGuh2);
+        public void PlayVO_Laugh() => PlayVoiceOver(voLaugh);
+        public void PlayVO_Mhm1() => PlayVoiceOver(voMhm1);
+        public void PlayVO_Mhm2() => PlayVoiceOver(voMhm2);
+        public void PlayVO_Mm1() => PlayVoiceOver(voMm1);
+        public void PlayVO_Mm2() => PlayVoiceOver(voMm2);
+        public void PlayVO_Mm3() => PlayVoiceOver(voMm3);
+        public void PlayVO_Mmmm() => PlayVoiceOver(voMmmm);
+        public void PlayVO_MmQ() => PlayVoiceOver(voMmQ);
+        public void PlayVO_Scoff() => PlayVoiceOver(voScoff);
+        public void PlayVO_Sigh() => PlayVoiceOver(voSigh);
         #endregion
     }
 }
