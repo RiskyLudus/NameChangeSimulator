f9438dfd8909eb93ffd28b60583fde311d936b0c
Aina M. <49330079+NeoGC@users.noreply.github.com>
2025-02-09 15:06:16 -0500

    Character Limits
    First/Middle/Last name text boxes for Dead Name and Actual Name inputs is 16 characters
    
    All others are 32 characters

--- Assets/_Constructs/Dialogue/Code/InputBox/DeadNameInputBoxController.cs
+++ Assets/_Constructs/Dialogue/Code/InputBox/DeadNameInputBoxController.cs
@@ -14,9 +14,13 @@ namespace NameChangeSimulator.Constructs.Dialogue.InputBox
         public void DisplayDeadNameInputWindow()
         {
             Debug.Log("Showing input window");
+            // I hate doing individual char limits, but if you have a better idea, I'm all ears. -Ai
             firstNameInputField.text = string.Empty;
+                firstNameInputField.characterLimit = 16; 
             middleNameInputField.text = string.Empty;
+                middleNameInputField.characterLimit = 16;
             lastNameInputField.text = string.Empty;
+                lastNameInputField.characterLimit = 16;
             container.SetActive(true);
         }
         
