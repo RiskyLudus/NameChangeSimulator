09c8486287b67ce2be53a9aef3561aa4ee741e02
Riza "Risky" Resnick <riza@nysom.com>
2025-06-13 13:13:57 -0700

    Have the audio sliders first pull from the mixer in case of a restart

--- Assets/_Constructs/Options/Code/OptionsController.cs
+++ Assets/_Constructs/Options/Code/OptionsController.cs
@@ -10,24 +10,38 @@ namespace NameChangeSimulator.Constructs.Options
     {
         [SerializeField] private GameObject optionsMenu;
         [SerializeField] private AudioMixer mixer;
-        
+
         [SerializeField] private Slider musicVolumeSlider;
         [SerializeField] private Slider sfxVolumeSlider;
         [SerializeField] private Slider voVolumeSlider;
 
-        public void ChangeVoiceOverVolume(float slidervalue)
+        void Start()
         {
-            mixer.SetFloat("VOVolume", Mathf.Log10(slidervalue) * 20);
-        }
-        
-        public void ChangeMusicVolume(float slidervalue)
-        {
-            mixer.SetFloat("MusicVolume", Mathf.Log10(slidervalue) * 20);
+            float value;
+
+            if (mixer.GetFloat("MusicVolume", out value))
+                musicVolumeSlider.value = Mathf.Pow(10f, value / 20f);
+
+            if (mixer.GetFloat("SFXVolume", out value))
+                sfxVolumeSlider.value = Mathf.Pow(10f, value / 20f);
+
+            if (mixer.GetFloat("VOVolume", out value))
+                voVolumeSlider.value = Mathf.Pow(10f, value / 20f);
         }
 
-        public void ChangeSFXVolume(float slidervalue)
+        public void ChangeVoiceOverVolume(float sliderValue)
         {
-            mixer.SetFloat("SFXVolume", Mathf.Log10(slidervalue) * 20);
+            mixer.SetFloat("VOVolume", Mathf.Log10(Mathf.Clamp(sliderValue, 0.0001f, 1f)) * 20);
+        }
+
+        public void ChangeMusicVolume(float sliderValue)
+        {
+            mixer.SetFloat("MusicVolume", Mathf.Log10(Mathf.Clamp(sliderValue, 0.0001f, 1f)) * 20);
+        }
+
+        public void ChangeSFXVolume(float sliderValue)
+        {
+            mixer.SetFloat("SFXVolume", Mathf.Log10(Mathf.Clamp(sliderValue, 0.0001f, 1f)) * 20);
         }
 
         public void ShowOptionsMenu()
